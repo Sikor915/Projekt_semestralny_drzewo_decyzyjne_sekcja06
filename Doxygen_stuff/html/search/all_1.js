a712b3ad62cb8798590e2159d56a760562b0b905
sikorgaming <sikorgaming@gmail.com>
2022-12-23 10:59:38 +0100

    Added Doxygen file to document all the future changes

---
+++ Doxygen_stuff/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['projekt_5fsemestralny_5fdrzewo_5fdecyzyjne_5fsekcja06_2ecpp_0',['Projekt_semestralny_drzewo_decyzyjne_sekcja06.cpp',['../_projekt__semestralny__drzewo__decyzyjne__sekcja06_8cpp.html',1,'']]]
+];
